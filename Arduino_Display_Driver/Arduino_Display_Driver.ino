5ef1515d327e9a188b39ecb4668a8cda04a3e471
= <=>
2020-03-08 23:03:30 +0100

    2nd Commit

--- Arduino_Display_Driver/Arduino_Display_Driver.ino
+++ Arduino_Display_Driver/Arduino_Display_Driver.ino
@@ -1,10 +1,58 @@
+#include <Adafruit_NeoPixel.h>
+
+#define displayWidth 10
+#define displayHight 20
+#define baudrate 19200
+
+void establishConnection();
+void exchangeInfo();
+bool checkConnection();
+
+typedef struct{
+  short id;
+  short red;
+  short green;
+  short blue;
+  }pixel;
+
+//mit folgenden Pointer bitte die Operationen machen:
+//mein Plan: jedes Pixel ist im Array gespeichert und wir updaten die Matrix anhand des Arrays mit einem delay für die Wiederholrate
+pixel *currentDisplay = malloc(sizeof(pixel) * displayWidth * displayHight);
+
 void setup() {
   // put your setup code here, to run once:
-
-
+ 
+  Serial.begin(baudrate);
+  establishConnection();
+  exchangeInfo();
 }
+
 
 void loop() {
   // put your main code here, to run repeatedly:
+  //Wie die Main()
+}
 
+void establishConnection(){
+  //Baut die Verbindung auf
+  if (Serial){
+    while(Serial.available() == 0){
+        Serial.println("SYN");
+        delay(100);
+    }
+    Serial.println("ACK");
+  }
+}
+
+void exchangeInfo(){
+  //Größe des Displays
+  Serial.println(String(displayWidth));
+  Serial.println(String(displayHight));
+}
+
+bool checkConnection(){
+  //Für Überprüfung 
+  
+  
+  return true;
 }
--- Raspberry/Display.py
+++ Raspberry/Display.py
@@ -1,4 +1,27 @@
 import serial
 
+class display:
+
+
+    def __init__(self, port, rate):
+        self.port = port
+        self.rate = rate
+        self.displayWidth = 0
+        self.displayHight = 0
+        display.serialOpen(self)
+        print(str(self.displayHight))
+        print(str(self.displayWidth))
+
+    def serialOpen(self):
+        with serial.Serial('COM3', 19200) as serialCon:
+            while serialCon.is_open:
+                serialCon.write(b'SYNACK')
+                if b'ACK' in serialCon.readline():
+                    self.displayWidth = int(serialCon.readline().decode('utf-8'))
+                    self.displayHight = int(serialCon.readline().decode('utf-8'))
+                    break
+
+
 if __name__ == '__main__':
-    print("Test")
+    disp = display('COM3', 19200)
+
