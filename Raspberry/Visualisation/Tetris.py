541ad23fdcfa314aa917cd05fd1514b736f69798
MagnavoxTG <dominik.lohner92@gmx.at>
2020-04-25 23:00:02 +0200

    Cleaned up the code and added colors for individual colors for Tetrominos

--- Raspberry/Visualisation/Tetris.py
+++ Raspberry/Visualisation/Tetris.py
@@ -10,68 +10,62 @@ class Tetris:
     Input.allowed_keys(["w", "a", "s", "d"])
 
     def __init__(self):
-        self.pixel = [numpy.array([[-1], [-1], [-1], [-1]])]
+        self.active_pixels = []
         self.display_height = Tetris.Display.get_height()
         self.display_width = Tetris.Display.get_width()
         self.Input.listener_start()
 
     def check_rows(self):
-        rows = []
+        full_rows = []
+        pixels_to_remove = []
+
         for row in range(self.display_height):
             active_pixels = 0
-            for pixel in self.pixel:
-                if pixel[1] == row:
+            pixels_to_remove_temp = []
+
+            for active_pixel in self.active_pixels:
+                if active_pixel[1] == row:
                     active_pixels += 1
-                if active_pixels >= self.display_width:
-                    rows.append(row)
+                    pixels_to_remove_temp.append(active_pixel)
+                if active_pixels == self.display_width:
+                    pixels_to_remove.extend(pixels_to_remove_temp)
+                    full_rows.append(row)
                     break
-        if rows:
-            rows.sort()
-            self.remove_rows(rows)
+
+        if full_rows:
+            self.remove_rows(full_rows, pixels_to_remove)
             return True
 
         return False
 
-    def remove_rows(self, rows):
-        pixels_to_remove = []
-
-        for row in rows:
-            for pixel in self.pixel:
-                if pixel[1] == row:
-                    pixels_to_remove.append(pixel)
-
-        for pixel in pixels_to_remove:
-            self.remove_array_from_list(self.pixel, pixel)
-            Tetris.Display.del_pixel(pixel)
+    def remove_rows(self, rows, pixels_to_remove):
+        for active_pixel in pixels_to_remove:
+            self.remove_array_from_list(self.active_pixels, active_pixel)
+            Tetris.Display.del_pixel(active_pixel)
             Tetris.Display.print_pixels()
-            time.sleep(0.05)
+            time.sleep(0.025)
 
-        for row in rows:
-            for number in range(len(self.pixel)):
-                if int(self.pixel[number][1]) > rows[0]:
-                    self.pixel[number] = numpy.subtract(self.pixel[number], numpy.array([[0], [1], [0], [0]]))
-
-        Tetris.Display.clear_screen()
-
-        for pixel in self.pixel:
-            Tetris.Display.add_pixel(pixel)
-
+        for number, active_pixel in enumerate(self.active_pixels):
+            if int(active_pixel[1]) > rows[0]:
+                Tetris.Display.del_pixel(active_pixel)
+                self.active_pixels[number] = numpy.subtract(active_pixel, numpy.array([[0], [len(rows)], [0], [0]]))
         Tetris.Display.print_pixels()
 
-        self.check_rows()
+        for active_pixel in self.active_pixels:
+            if int(active_pixel[1]) >= rows[0]:
+                Tetris.Display.add_pixel(active_pixel)
+        Tetris.Display.print_pixels()
 
     def check_collision(self, pixel):
         if not 0 <= pixel[0] < self.display_width or not 0 <= pixel[1]:
             return True
 
-        if not 0 <= pixel[1]:
-            return True
-
-        if self.pixel:
-            for pixels in self.pixel:
-                if pixel[0] == pixels[0] and pixel[1] == pixels[1]:
+        if self.active_pixels:
+            for active_pixel in self.active_pixels:
+                if pixel[0] == active_pixel[0] and pixel[1] == active_pixel[1]:
                     return True
             return False
+        return False
 
     @staticmethod
     def remove_array_from_list(lst, arr):
@@ -105,49 +99,51 @@ class Tetromino:
     @staticmethod
     def starting_position(piece_symbol, display_height, display_width):
         if piece_symbol == "I":
-            return [numpy.array([[(display_width//2)+0], [display_height-2], [1], [255]]),
-                    numpy.array([[(display_width//2)-1], [display_height-2], [1], [255]]),
-                    numpy.array([[(display_width//2)+1], [display_height-2], [1], [255]]),
-                    numpy.array([[(display_width//2)+2], [display_height-2], [1], [255]])]
+            return [numpy.array([[(display_width//2)+0], [display_height-2], [1], [44991]]),
+                    numpy.array([[(display_width//2)-1], [display_height-2], [1], [44991]]),
+                    numpy.array([[(display_width//2)+1], [display_height-2], [1], [44991]]),
+                    numpy.array([[(display_width//2)+2], [display_height-2], [1], [44991]])]
         elif piece_symbol == "O":
-            return [numpy.array([[(display_width//2)+0], [display_height-2], [1], [255]]),
-                    numpy.array([[(display_width//2)+0], [display_height-1], [1], [255]]),
-                    numpy.array([[(display_width//2)+1], [display_height-2], [1], [255]]),
-                    numpy.array([[(display_width//2)+1], [display_height-1], [1], [255]])]
+            return [numpy.array([[(display_width//2)+0], [display_height-2], [1], [12230656]]),
+                    numpy.array([[(display_width//2)+0], [display_height-1], [1], [12230656]]),
+                    numpy.array([[(display_width//2)+1], [display_height-2], [1], [12230656]]),
+                    numpy.array([[(display_width//2)+1], [display_height-1], [1], [12230656]])]
         elif piece_symbol == "T":
-            return [numpy.array([[(display_width//2)+0], [display_height-2], [1], [255]]),
-                    numpy.array([[(display_width//2)+0], [display_height-1], [1], [255]]),
-                    numpy.array([[(display_width//2)-1], [display_height-2], [1], [255]]),
-                    numpy.array([[(display_width//2)+1], [display_height-2], [1], [255]])]
+            return [numpy.array([[(display_width//2)+0], [display_height-2], [1], [12517575]]),
+                    numpy.array([[(display_width//2)+0], [display_height-1], [1], [12517575]]),
+                    numpy.array([[(display_width//2)-1], [display_height-2], [1], [12517575]]),
+                    numpy.array([[(display_width//2)+1], [display_height-2], [1], [12517575]])]
         elif piece_symbol == "S":
-            return [numpy.array([[(display_width//2)+0], [display_height-2], [1], [255]]),
-                    numpy.array([[(display_width//2)-1], [display_height-2], [1], [255]]),
-                    numpy.array([[(display_width//2)+0], [display_height-1], [1], [255]]),
-                    numpy.array([[(display_width//2)+1], [display_height-1], [1], [255]])]
+            return [numpy.array([[(display_width//2)+0], [display_height-2], [1], [98560]]),
+                    numpy.array([[(display_width//2)-1], [display_height-2], [1], [98560]]),
+                    numpy.array([[(display_width//2)+0], [display_height-1], [1], [98560]]),
+                    numpy.array([[(display_width//2)+1], [display_height-1], [1], [98560]])]
         elif piece_symbol == "Z":
-            return [numpy.array([[(display_width//2)+0], [display_height-2], [1], [255]]),
-                    numpy.array([[(display_width//2)-1], [display_height-1], [1], [255]]),
-                    numpy.array([[(display_width//2)+0], [display_height-1], [1], [255]]),
-                    numpy.array([[(display_width//2)+1], [display_height-2], [1], [255]])]
+            return [numpy.array([[(display_width//2)+0], [display_height-2], [1], [13443073]]),
+                    numpy.array([[(display_width//2)-1], [display_height-1], [1], [13443073]]),
+                    numpy.array([[(display_width//2)+0], [display_height-1], [1], [13443073]]),
+                    numpy.array([[(display_width//2)+1], [display_height-2], [1], [13443073]])]
         elif piece_symbol == "J":
-            return [numpy.array([[(display_width//2)+0], [display_height-2], [1], [255]]),
-                    numpy.array([[(display_width//2)-1], [display_height-1], [1], [255]]),
-                    numpy.array([[(display_width//2)-1], [display_height-2], [1], [255]]),
-                    numpy.array([[(display_width//2)+1], [display_height-2], [1], [255]])]
+            return [numpy.array([[(display_width//2)+0], [display_height-2], [1], [171]]),
+                    numpy.array([[(display_width//2)-1], [display_height-1], [1], [171]]),
+                    numpy.array([[(display_width//2)-1], [display_height-2], [1], [171]]),
+                    numpy.array([[(display_width//2)+1], [display_height-2], [1], [171]])]
         elif piece_symbol == "L":
-            return [numpy.array([[(display_width//2)+0], [display_height-2], [1], [255]]),
-                    numpy.array([[(display_width//2)-1], [display_height-2], [1], [255]]),
-                    numpy.array([[(display_width//2)+1], [display_height-2], [1], [255]]),
-                    numpy.array([[(display_width//2)+1], [display_height-1], [1], [255]])]
+            return [numpy.array([[(display_width//2)+0], [display_height-2], [1], [16092696]]),
+                    numpy.array([[(display_width//2)-1], [display_height-2], [1], [16092696]]),
+                    numpy.array([[(display_width//2)+1], [display_height-2], [1], [16092696]]),
+                    numpy.array([[(display_width//2)+1], [display_height-1], [1], [16092696]])]
 
     def gravity(self, tetris: Tetris):
-        for pos in range(len(self.position)):
-            temp = numpy.subtract(self.position[pos], numpy.array([[0], [1], [0], [0]]))
+        for pos in self.position:
+            temp = numpy.subtract(pos, numpy.array([[0], [1], [0], [0]]))
+
             if tetris.check_collision(temp):
-                for pixels in self.position:
-                    pixels[3] = 16711680
-                    tetris.pixel.append(pixels)
-                    Tetris.Display.add_pixel(pixels)
+                for pos2 in self.position:
+                    Tetris.Display.del_pixel(pos2)
+                    pos2[3] = 9342606
+                    tetris.active_pixels.append(pos2)
+                    Tetris.Display.add_pixel(pos2)
                 Tetris.Display.print_pixels()
                 tetris.check_rows()
                 return Tetromino(tetris)
@@ -170,7 +166,7 @@ class Tetromino:
     def rotate(self, tetris: Tetris):
         # It is pointless to rotate this piece
         if self.piece == "O":
-            return False
+            return True
 
         # for translating the pixels to 0/0 in the coordinate system
         # the first pixel of every Tetromino is the reference point for the rotation
@@ -186,10 +182,10 @@ class Tetromino:
                                                  [0, 0, 1, 0],
                                                  [0, 0, 0, 1]])
 
-        rotation_matrix = numpy.array([[0, 1, 0, 0],
+        rotation_matrix = numpy.array([[-0, 1, 0, 0],
                                        [-1, 0, 0, 0],
-                                       [0, 0, 1, 0],
-                                       [0, 0, 0, 1]])
+                                       [-0, 0, 1, 0],
+                                       [-0, 0, 0, 1]])
 
         # checks if the new pixel is valid (not colliding with anything)
         for pos in self.position:
@@ -218,41 +214,30 @@ class Tetromino:
         Tetris.Display.print_pixels()
 
     def move(self, key, tetris: Tetris):
+        direction = 0
+
         if key == "a":
-            for pos in range(len(self.position)):
-                temp = numpy.subtract(self.position[pos], numpy.array([[1], [0], [0], [0]]))
-                if tetris.check_collision(temp):
-                    return False
-
-            # deletes old pixel state
-            for pos in self.position:
-                Tetris.Display.del_pixel(pos)
-            Tetris.Display.print_pixels()
-
-            for pos in range(len(self.position)):
-                self.position[pos] = numpy.subtract(self.position[pos], numpy.array([[1], [0], [0], [0]]))
-
-            # shows new pixels
-            for pos in self.position:
-                Tetris.Display.add_pixel(pos)
-            Tetris.Display.print_pixels()
-
+            direction = 1
         if key == "d":
-            for pos in range(len(self.position)):
-                temp = numpy.subtract(self.position[pos], numpy.array([[-1], [0], [0], [0]]))
-                if tetris.check_collision(temp):
-                    return False
+            direction = -1
 
-            for pos in self.position:
-                Tetris.Display.del_pixel(pos)
-            Tetris.Display.print_pixels()
+        for pos in self.position:
+            temp = numpy.subtract(pos, numpy.array([[direction], [0], [0], [0]]))
+            if tetris.check_collision(temp):
+                return False
 
-            for pos in range(len(self.position)):
-                self.position[pos] = numpy.subtract(self.position[pos], numpy.array([[-1], [0], [0], [0]]))
+        # deletes old pixel state
+        for pos in self.position:
+            Tetris.Display.del_pixel(pos)
+        Tetris.Display.print_pixels()
 
-            for pos in self.position:
-                Tetris.Display.add_pixel(pos)
-            Tetris.Display.print_pixels()
+        for pos in range(len(self.position)):
+            self.position[pos] = numpy.subtract(self.position[pos], numpy.array([[direction], [0], [0], [0]]))
+
+        # shows new pixels
+        for pos in self.position:
+            Tetris.Display.add_pixel(pos)
+        Tetris.Display.print_pixels()
 
 
 if __name__ == '__main__':
