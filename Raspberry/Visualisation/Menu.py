8567109cd20aaf2a28c13cb703085803c77236ef
= <=>
2020-05-12 13:06:23 +0200

    -->working on Menu

--- Raspberry/Visualisation/Menu.py
+++ Raspberry/Visualisation/Menu.py
@@ -1,6 +1,6 @@
 from numpy import array
 from kivymd.uix.toolbar import MDToolbar
-from kivy.uix.screenmanager import Screen
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.anchorlayout import AnchorLayout
 from kivymd.app import MDApp
 from kivymd.uix.button import MDRectangleFlatButton
@@ -21,52 +21,56 @@ BoxLayout:
         MDCard:
             size_hint: None, None
             size: "280dp", "180dp"
-            
-            MDLabel:
-                text: "Tetris"
-                theme_text_color: "Primary"
-                halign: "center"
-                
         
-        MDCard:
-            size_hint: None, None
-            size: "280dp", "180dp"
-            
-        MDCard:
-            size_hint: None, None
-            size: "280dp", "180dp"
-            
-        MDCard:
-            size_hint: None, None
-            size: "280dp", "180dp"
-            
+            BoxLayout:
+                orientation: "vertical"
+                
+                MDLabel:
+                    text: "Tetris"
+                    theme_text_color: "Primary"
+                    halign: "center"
+                
+                Image:
+                    source: "A:/Dionysos/tetris.jpg"
+                    allow_stretch: True
+                    keep_ratio: False
+                
+                BoxLayout:
+                    
+                    MDRectangleFlatIconButton:
+                        icon: "play"
+                        text: "Play"
+                        width: dp(140)
+                        on_press: root.start_game()
+                    
+                    MDRectangleFlatIconButton:
+                        icon: "trophy"
+                        text: "Highscore"
+                        width: dp(140)
 
-    '''
+        MDCard:
+            size_hint: None, None
+            size: "280dp", "180dp"
+            
+        MDCard:
+            size_hint: None, None
+            size: "280dp", "180dp"
+            
+        MDCard:
+            size_hint: None, None
+            size: "280dp", "180dp"
+            
+'''
 
 
 class MainApp(MDApp):
     def build(self):
         self.theme_cls.theme_style = "Dark"
-        self.theme_cls.primary_palette = "LightBlue"
-        """screen = Screen()
-
-        screen.add_widget(
-            MDToolbar(title="DIONYSOS", type="top")
-        )
-        screen.add_widget(
-            MDRectangleFlatButton(
-                text="Hello, World",
-                pos_hint={"center_x": 0.5, "center_y": 0.5},
-            )
-        )
-        screen.add_widget(
-            MDRectangleFlatButton(
-                text="Test",
-                pos_hint={"center_x": 0.2, "center_y": 0.5},
-            )
-        )"""
+        self.theme_cls.primary_palette = "LightBue"
 
         return Builder.load_string(KV)
 
+    def start_game(self):
+        print("geht")
 
 MainApp().run()
