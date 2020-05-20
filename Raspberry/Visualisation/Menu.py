949e0cfc7ca001d28ba39f93a3fb0ea563c5569a
= <=>
2020-05-20 20:42:08 +0200

    -->Menu basic functionality

--- Raspberry/Visualisation/Menu.py
+++ Raspberry/Visualisation/Menu.py
@@ -1,10 +1,6 @@
-from numpy import array
-from kivymd.uix.toolbar import MDToolbar
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.anchorlayout import AnchorLayout
 from kivymd.app import MDApp
-from kivymd.uix.button import MDRectangleFlatButton
 from kivy.lang import Builder
+from subprocess import call
 
 KV = '''
 BoxLayout:
@@ -18,10 +14,11 @@ BoxLayout:
         cols: 3
         spacing: 10, 10
         
+        
         MDCard:
             size_hint: None, None
             size: "280dp", "180dp"
-        
+            
             BoxLayout:
                 orientation: "vertical"
                 
@@ -41,7 +38,7 @@ BoxLayout:
                         icon: "play"
                         text: "Play"
                         width: dp(140)
-                        on_press: root.start_game()
+                        on_press: app.start_game('t')
                     
                     MDRectangleFlatIconButton:
                         icon: "trophy"
@@ -51,6 +48,33 @@ BoxLayout:
         MDCard:
             size_hint: None, None
             size: "280dp", "180dp"
+
+            
+            BoxLayout:
+                orientation: "vertical"
+                
+                MDLabel:
+                    text: "Snake"
+                    theme_text_color: "Primary"
+                    halign: "center"
+                
+                Image:
+                    source: "A:/Dionysos/tetris.jpg"
+                    allow_stretch: True
+                    keep_ratio: False
+                
+                BoxLayout:
+                    
+                    MDRectangleFlatIconButton:
+                        icon: "play"
+                        text: "Play"
+                        width: dp(140)
+                        on_press: app.start_game('s')
+                    
+                    MDRectangleFlatIconButton:
+                        icon: "trophy"
+                        text: "Highscore"
+                        width: dp(140)
             
         MDCard:
             size_hint: None, None
@@ -59,18 +83,21 @@ BoxLayout:
         MDCard:
             size_hint: None, None
             size: "280dp", "180dp"
-            
+                
 '''
 
 
 class MainApp(MDApp):
     def build(self):
         self.theme_cls.theme_style = "Dark"
-        self.theme_cls.primary_palette = "LightBue"
-
+        self.theme_cls.primary_palette = "LightBlue"
         return Builder.load_string(KV)
 
-    def start_game(self):
-        print("geht")
+    def start_game(self, game):
+        if game == 't':
+            call('Tetris.py', shell=True)
+        elif game == 's':
+            call('Snake.py', shell=True)
+
 
 MainApp().run()
